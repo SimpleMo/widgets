97ea9e2dcc1db6e1b19f2c41de1524847027b649
VRusakov <vrusakov@lanit.ru>
2019-11-04 17:34:51 +0500

    Initial commit

---
+++ src/test/java/org/miro/test/widgets/WidgetsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.miro.test.widgets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WidgetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
