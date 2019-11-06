86a2624219d25f0d4e4b927527d25099c9b53834
VRusakov <vrusakov@lanit.ru>
2019-11-06 18:40:18 +0500

    Добавлен контроллер

---
+++ src/main/java/org/miro/test/widgets/controller/WidgetsController.java
@@ -0,0 +1,45 @@
+package org.miro.test.widgets.controller;
+
+import org.miro.test.widgets.model.Widget;
+import org.miro.test.widgets.service.WidgetsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("widgets")
+public class WidgetsController {
+
+    @Autowired
+    protected WidgetsService widgetsService;
+
+    @GetMapping
+    public List<Widget> getWidgets(){
+        List<Widget> widgets = getWidgetList();
+        return widgets;
+    }
+
+    @GetMapping("{uuid}")
+    public Widget getWidget(@PathVariable UUID uuid){
+        return widgetsService.findByUuid(uuid);
+    }
+
+    private List<Widget> getWidgetList() {
+        Widget widget = new Widget();
+        widget.setX(1L);
+        widget.setY(1L);
+        widget.setHeight(2L);
+        widget.setWidth(2L);
+        widget.setzIndex(1L);
+        List<Widget> widgets = new ArrayList<>();
+        widgets.add(widget);
+        return widgets;
+    }
+
+}
